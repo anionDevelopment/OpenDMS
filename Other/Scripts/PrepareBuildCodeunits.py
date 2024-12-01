2d3cac399ebf2c2180f79ab595258f5c46571121
Marius Göcke <marius.goecke@gmail.com>
2024-12-02 00:04:17 +0100

    fix

--- Other/Scripts/PrepareBuildCodeunits.py
+++ Other/Scripts/PrepareBuildCodeunits.py
@@ -1,11 +1,13 @@
 from pathlib import Path
+from ScriptCollection.GeneralUtilities import GeneralUtilities
 from ScriptCollection.TasksForCommonProjectStructure import TasksForCommonProjectStructure
 
 
 def prepare_build_codeunits():
     t = TasksForCommonProjectStructure()
     current_file = str(Path(__file__).absolute())
-    t.ensure_certificate_authority_for_development_purposes_is_generated(current_file)
+    product_folder: str = GeneralUtilities.resolve_relative_path("../../..", current_file)
+    t.ensure_certificate_authority_for_development_purposes_is_generated(product_folder)
 
 
 if __name__ == "__main__":
