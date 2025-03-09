d5c888ca4d7ef7f00ae11720b7df4fc872b1c1d5
Marius Göcke <marius.goecke@gmail.com>
2025-03-09 02:29:05 +0100

    updated build.py and diagrams

--- OpenDMS/Other/Build/Build.py
+++ OpenDMS/Other/Build/Build.py
@@ -4,7 +4,11 @@ from ScriptCollection.TasksForCommonProjectStructure import TasksForCommonProjec
 
 
 def build():
-    TasksForCommonProjectStructure().standardized_tasks_build_for_docker_project(str(Path(__file__).absolute()), "QualityCheck", 1, sys.argv)
+    t = TasksForCommonProjectStructure()
+    build_script_file = str(Path(__file__).absolute())
+    t.standardized_tasks_build_for_docker_project(build_script_file, "QualityCheck", 1, sys.argv)
+    t.merge_sbom_file_from_dependent_codeunit_into_this(build_script_file, "OpendMSBackend")
+    t.merge_sbom_file_from_dependent_codeunit_into_this(build_script_file, "OpendMSFrontend")
 
 
 if __name__ == "__main__":
