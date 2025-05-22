7932c343452a68308cf92af16a418a4a17551d9f
Marius Göcke <marius.goecke@gmail.com>
2025-05-22 17:03:01 +0200

    update for update-dependencies

---
+++ OpenDMS/Other/Reference/ReferenceContent/Examples/MinimalDockerComposeFile/MinimalDockerComposeFile.md
@@ -0,0 +1,19 @@
+# Minimal docker-compose-file
+
+## Access
+
+### Database
+
+connectionstring: `Server=opendms_database;Port=3306;Database=OpenDMSDatabase;UID=root;PWD=R00tpa55w0rd;`
+
+Data for adminer:
+
+System: `MySQL`
+
+Server: `opendms_database`
+
+Username: `root`
+
+Password: `R00tpa55w0rd`
+
+Database: `OpenDMSDatabase`
--- Other/Scripts/UpdateDependencies.py
+++ Other/Scripts/UpdateDependencies.py
@@ -2,13 +2,23 @@ from pathlib import Path
 from ScriptCollection.GeneralUtilities import GeneralUtilities
 from ScriptCollection.TasksForCommonProjectStructure import TasksForCommonProjectStructure
 
+
+def update_images_in_example(repository_folder: str):
+    iu = ImageUpdater()
+    iu.add_default_mapper()
+    dockercomposefile: str = f"{repository_folder}\\Other\\Reference\\ReferenceContent\\Examples\\MinimalDockerComposeFile\\docker-compose.yml"
+    excluded = ["opendms"]
+    iu.update_all_services_in_docker_compose_file(dockercomposefile, VersionEcholon.LatestPatchOrLatestMinor, excluded)
+    iu.check_for_newest_version(dockercomposefile, excluded)
+
+
 def update_dependencies():
     current_file = str(Path(__file__).absolute())
     repository_folder = GeneralUtilities.resolve_relative_path("../../..", current_file)
-    t:TasksForCommonProjectStructure=TasksForCommonProjectStructure()
-    t.update_submodule(repository_folder,"tessdata_best")
+    t: TasksForCommonProjectStructure = TasksForCommonProjectStructure()
+    t.update_submodule(repository_folder, "tessdata_best")
+    update_images_in_example(repository_folder)
 
 
 if __name__ == "__main__":
     update_dependencies()
- 
