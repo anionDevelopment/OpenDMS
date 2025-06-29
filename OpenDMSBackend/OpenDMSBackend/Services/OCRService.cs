273bf8dcfbc68b033d2556090a07b819931d4f4a
Marius Göcke <marius.goecke@gmail.com>
2025-06-29 15:40:34 +0200

    fixes

--- OpenDMSBackend/OpenDMSBackend/Services/OCRService.cs
+++ OpenDMSBackend/OpenDMSBackend/Services/OCRService.cs
@@ -9,6 +9,7 @@ namespace OpenDMSBackend.Core.Services
         {
             string result = string.Empty;
             //from https://github.com/charlesw/tesseract-samples/blob/master/src/Tesseract.ConsoleDemo/Program.cs
+            /*
             using (TesseractEngine engine = new TesseractEngine(@"./OCRData", "eng+deu", EngineMode.Default))//TODO make the languages confgurable
             {
                 using Pix img = Pix.LoadFromMemory(content);
@@ -16,6 +17,8 @@ namespace OpenDMSBackend.Core.Services
                 result = result + Environment.NewLine + page.GetText();
             }
             return result;
+            */
+            return "";//TODO
         }
     }
 }
