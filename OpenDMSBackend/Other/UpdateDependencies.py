e48245b7ea294d71d0ea29b9c8217414d3bca650
Marius Göcke <marius.goecke@gmail.com>
2025-09-28 15:56:54 +0200

    updated buidlscripts

--- OpenDMSBackend/Other/UpdateDependencies.py
+++ OpenDMSBackend/Other/UpdateDependencies.py
@@ -2,15 +2,15 @@ import sys
 import os
 from pathlib import Path
 from ScriptCollection.GeneralUtilities import GeneralUtilities
-from ScriptCollection.TasksForCommonProjectStructure import TasksForCommonProjectStructure
+import sys
 
+from ScriptCollection.TFCPS.DotNet.TFCPS_CodeUnitSpecific_DotNet import TFCPS_CodeUnitSpecific_DotNet_Functions,TFCPS_CodeUnitSpecific_DotNet_CLI
+ 
 
 def update_dependencies():
-    t = TasksForCommonProjectStructure()
-    script_file: str = str(Path(__file__).absolute())
-    t.update_dependencies_of_typical_dotnet_codeunit(script_file, 1, sys.argv)
-    codeunit_folder = GeneralUtilities.resolve_relative_path("..", os.path.dirname(script_file))
-    t.update_dependencies_of_typical_python_repository_requirements(codeunit_folder, 1, sys.argv)
+    tf:TFCPS_CodeUnitSpecific_DotNet_Functions=TFCPS_CodeUnitSpecific_DotNet_CLI.parse(__file__)
+    tf.update_dependencies()
+    #TODO t.update_dependencies_of_typical_python_repository_requirements(codeunit_folder, 1, sys.argv)
 
 
 if __name__ == "__main__":
--- Other/Scripts/PrepareBuildCodeunits.py
+++ Other/Scripts/PrepareBuildCodeunits.py
@@ -1,20 +1,18 @@
-from pathlib import Path
 import os
+from pathlib import Path
 from ScriptCollection.GeneralUtilities import GeneralUtilities
-from ScriptCollection.TasksForCommonProjectStructure import TasksForCommonProjectStructure
-
-
+from ScriptCollection.TFCPS.TFCPS_Generic import TFCPS_Generic_Functions, TFCPS_Generic_CLI
+    
 def prepare_build_codeunits():
-    t = TasksForCommonProjectStructure()
-    current_file = str(Path(__file__).absolute())
-    repository_folder = GeneralUtilities.resolve_relative_path("../../..", current_file)
-    t.clone_repository_as_resource(repository_folder, "https://github.com/tesseract-ocr/tessdata", "OCRData", "tessdata")
-    t.ensure_certificate_authority_for_development_purposes_is_generated(repository_folder)
-    t.generate_certificate_for_development_purposes_for_product(repository_folder)
-    t.generate_tasksfile_from_workspace_file(repository_folder)
-    t.generate_codeunits_overview_diagram(repository_folder)
-    t.generate_svg_files_from_plantuml_files_for_repository(repository_folder)
-    t.do_npm_install(os.path.join(repository_folder, "Other", "Resources", "TypeScript"), True, 1)
+    t :TFCPS_Generic_Functions= TFCPS_Generic_CLI().parse(__file__)
+    repository_folder = GeneralUtilities.resolve_relative_path( "../../..", str(Path(__file__).absolute()))
+    t.tfcps_Tools_General.clone_repository_as_resource(repository_folder, "https://github.com/tesseract-ocr/tessdata", "OCRData", "tessdata")
+    t.tfcps_Tools_General.ensure_certificate_authority_for_development_purposes_is_generated(repository_folder)
+    t.tfcps_Tools_General.generate_certificate_for_development_purposes_for_product(repository_folder)
+    t.tfcps_Tools_General.generate_tasksfile_from_workspace_file(repository_folder)
+    t.tfcps_Tools_General.generate_codeunits_overview_diagram(repository_folder)
+    t.tfcps_Tools_General.generate_svg_files_from_plantuml_files_for_repository(repository_folder)
+    t.tfcps_Tools_General.do_npm_install(os.path.join(repository_folder, "Other", "Resources", "TypeScript"))
 
 
 if __name__ == "__main__":
