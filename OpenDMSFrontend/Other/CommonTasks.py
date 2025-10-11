2b336c5ee58ef50b5c7f49325391eefa869ca47f
Marius Göcke <marius.goecke@gmail.com>
2025-10-11 14:57:14 +0200

    fix

--- OpenDMSFrontend/Other/CommonTasks.py
+++ OpenDMSFrontend/Other/CommonTasks.py
@@ -2,9 +2,9 @@ from ScriptCollection.TFCPS.NodeJS.TFCPS_CodeUnitSpecific_NodeJS import TFCPS_Co
 
 
 def common_tasks():
-    tf:TFCPS_CodeUnitSpecific_NodeJS_Functions=TFCPS_CodeUnitSpecific_NodeJS_CLI.parse(__file__)    
+    tf:TFCPS_CodeUnitSpecific_NodeJS_Functions=TFCPS_CodeUnitSpecific_NodeJS_CLI.parse(__file__)
     tf.do_common_tasks(tf.get_version_of_project())#codeunit-version should alsways be the same as project-version
-    tf.tfcps_Tools_General.generate_api_client_from_dependent_codeunit_for_angular(tf.get_codeunit_folder(),"OpenDMSBackend","open-dms-backend", "typescript-angular",tf.use_cache())
+    tf.tfcps_Tools_General.generate_api_client_from_dependent_codeunit(tf.get_codeunit_folder(),"OpenDMSBackend","src/app/generated/open-dms-backend", "typescript-angular",tf.use_cache(),["apis","models","supportingFiles"])
  
 if __name__ == "__main__":
     common_tasks()
