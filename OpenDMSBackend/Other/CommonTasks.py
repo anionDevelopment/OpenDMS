868ee581821a401a79d9e0f97531a1de43d4f08b
Marius Göcke <marius.goecke@gmail.com>
2025-10-17 18:10:22 +0200

    updated version

--- OpenDMSBackend/Other/CommonTasks.py
+++ OpenDMSBackend/Other/CommonTasks.py
@@ -6,7 +6,8 @@ def common_tasks():
     tf:TFCPS_CodeUnitSpecific_DotNet_Functions=TFCPS_CodeUnitSpecific_DotNet_CLI.parse(__file__)    
     tf.tfcps_Tools_General.get_resource_from_global_resource(tf.get_codeunit_folder(), "OCRData")
     tf.tfcps_Tools_General.get_resource_from_global_resource(tf.get_codeunit_folder(), "DevelopmentCertificate")
-    tf.do_common_tasks(tf.get_version_of_project(),CertificateGeneratorInformationGenerate(),True)#codeunit-version should alsways be the same as project-version
+    tf.do_common_tasks(tf.get_version_of_project(),CertificateGeneratorInformationGenerate())#codeunit-version should alsways be the same as project-version
+    tf.tfcps_Tools_General.t4_transform(tf.get_codeunit_folder(),True,tf.use_cache())
 
 if __name__ == "__main__":
     common_tasks()
