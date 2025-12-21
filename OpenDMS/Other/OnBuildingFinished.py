e35f05d57490c690c09da1cad4a230be24603562
Marius Göcke <marius.goecke@gmail.com>
2025-12-21 21:22:15 +0100

    wip

--- OpenDMS/Other/OnBuildingFinished.py
+++ OpenDMS/Other/OnBuildingFinished.py
@@ -3,7 +3,7 @@ from ScriptCollection.TFCPS.Docker.TFCPS_CodeUnitSpecific_Docker import TFCPS_Co
 
 def on_building_finished():
     tf:TFCPS_CodeUnitSpecific_Docker_Functions=TFCPS_CodeUnitSpecific_Docker_CLI.parse(__file__)
-    tf.verify_image_is_working(None,None,443,"/API/Other/Maintenance/AvailabilityCheck",True)
+    tf.verify_image_is_working(None,{"InitialDatabaseType":"Transient"},443,"/API/Other/Maintenance/AvailabilityCheck",True)
  
 if __name__ == "__main__":
     on_building_finished()
--- Other/Scripts/PrepareBuildCodeunits.py
+++ Other/Scripts/PrepareBuildCodeunits.py
@@ -1,33 +1,5 @@
 import os
-from pathlib import Path
-from ScriptCollection.ScriptCollectionCore import ScriptCollectionCore
-from ScriptCollection.GeneralUtilities import GeneralUtilities
 from ScriptCollection.TFCPS.TFCPS_Generic import TFCPS_Generic_Functions, TFCPS_Generic_CLI
-
-@GeneralUtilities.check_arguments
-def get_docker_networks(sc:ScriptCollectionCore)->list[str]:
-    program_result=sc.run_program("docker","network list")
-    result=[]
-    lines=program_result[1].split("\n")[1:]
-    for line in lines:
-        splitted=[item for item in line.split(' ') if GeneralUtilities.string_has_content(item)]
-        result.append(splitted[1])
-    return result
-
-@GeneralUtilities.check_arguments
-def ensure_docker_network_is_available(t :TFCPS_Generic_Functions,network_name:str):
-    if not (network_name  in get_docker_networks(t.sc)):
-        t.sc.run_program("docker",f"network create {network_name}")
-
-@GeneralUtilities.check_arguments
-def pull_images_of_test_services(t :TFCPS_Generic_Functions):
-    test_services=[f for f in GeneralUtilities.get_direct_folders_of_folder(os.path.join(t.repository_folder,"Other","Resources","LocalTestServices"))]
-    if 0<len(test_services):
-       t.sc.log.log("Pull images for local test-services...")
-    for test_service_folder in test_services:
-        test_service_name=os.path.basename(test_service_folder)
-        t.sc.log.log(f"Pull image for test-service {test_service_name}...")
-        t.sc.run_program("docker",f"compose -f docker-compose.yml pull --quiet",test_service_folder,print_live_output=True)
 
 def prepare_build_codeunits():
     t :TFCPS_Generic_Functions= TFCPS_Generic_CLI().parse(__file__)
@@ -36,9 +8,9 @@ def prepare_build_codeunits():
     t.tfcps_Tools_General.generate_tasksfile_from_workspace_file(t.repository_folder)
     t.tfcps_Tools_General.generate_codeunits_overview_diagram(t.repository_folder)
     t.tfcps_Tools_General.generate_svg_files_from_plantuml_files_for_repository(t.repository_folder,t.use_cache())
+    t.sc.ensure_docker_network_is_available("opendms_net")
+    t.tfcps_Tools_General.pull_images_of_test_services(t.repository_folder)
     t.tfcps_Tools_General.do_npm_install(os.path.join(t.repository_folder, "Other", "Resources", "TypeScript"),True,t.use_cache())
-    ensure_docker_network_is_available(t,"opendms_net")
-    pull_images_of_test_services(t)
 
 
 if __name__ == "__main__":
