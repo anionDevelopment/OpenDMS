a26eff72c06f8218ec42558d574ead5598bb0de0
Marius Göcke <marius.goecke@gmail.com>
2025-12-21 09:48:58 +0100

    update

--- OpenDMS/Other/Build/Build.py
+++ OpenDMS/Other/Build/Build.py
@@ -3,7 +3,7 @@ from ScriptCollection.TFCPS.Docker.TFCPS_CodeUnitSpecific_Docker import TFCPS_Co
  
 def build():
     tf:TFCPS_CodeUnitSpecific_Docker_Functions=TFCPS_CodeUnitSpecific_Docker_CLI.parse(__file__)
-    tf.build()
+    tf.build(None)
     tf.tfcps_Tools_General.merge_sbom_file_from_dependent_codeunit_into_this(tf.get_codeunit_folder(),tf.get_codeunit_name(),"OpenDMSBackend",tf.use_cache())
     tf.tfcps_Tools_General.merge_sbom_file_from_dependent_codeunit_into_this(tf.get_codeunit_folder(),tf.get_codeunit_name(),"OpenDMSFrontend",tf.use_cache())
     #TODO verify it is working by running "docker run opendms ..." (transient persistence is enough) and verify after some seconds that the container is available and ready by some healthcheck or log check
