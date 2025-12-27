a20a3bb805fa4000d35ee14fd96e6fa64d0ec06e
Marius Göcke <marius.goecke@gmail.com>
2025-12-27 05:39:51 +0100

    updated healthcheck

--- OpenDMS/OpenDMS/HealthCheck.sh
+++ OpenDMS/OpenDMS/HealthCheck.sh
@@ -1,2 +1,13 @@
-curl --fail http://localhost:80/API/Other/Maintenance/HealthCheck
-#TODO extract to script and add frontend to healthcheck
+#!/bin/bash
+set -o errexit
+configfile="/Workspace/Configuration/Configuration.xml"
+protocol=$(sed -n 's/.*exs:type="ns10:\(HTTP\|HTTPS\)".*/\1/p' $configfile)
+protocol="${protocol,,}" # make protocol to lower
+port=$(sed -n 's/.*Port="\([0-9]\+\)".*/\1/p' $configfile)
+healthcheckcommand="curl --fail -v"
+if [ "$protocol" == "https" ]; then
+    healthcheckcommand="$healthcheckcommand --insecure"
+fi
+healthcheckcommand="$healthcheckcommand \"$protocol://127.0.0.1:$port/API/Other/Maintenance/HealthCheck\""
+echo "health-check-command: $healthcheckcommand"
+eval $healthcheckcommand
