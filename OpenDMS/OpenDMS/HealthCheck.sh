69c68840972a40950c33878601df30e901c49b0b
Marius Göcke <marius.goecke@gmail.com>
2025-12-30 21:06:11 +0100

    fixed healthcheck

--- OpenDMS/OpenDMS/HealthCheck.sh
+++ OpenDMS/OpenDMS/HealthCheck.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 set -o errexit
 configfile="/Workspace/Configuration/Configuration.xml"
-protocol=$(sed -n 's/.*exs:type="ns10:\(HTTP\|HTTPS\)".*/\1/p' $configfile)
+
+protocol=$(sed -n 's/.*<Protocol[^>]*exs:type="ns[0-9]\+:\([^"]*\)".*/\1/p' $configfile)
 protocol="${protocol,,}" # make protocol to lower
-port=$(sed -n 's/.*Port="\([0-9]\+\)".*/\1/p' $configfile)
+if [[ -z "$protocol" ]]; then
+  echo "Protocol for healthcheck not available."
+  exit 3
+fi
+
+port=$(sed -n 's/.*<Protocol[^>]*Port="\([^"]*\)".*/\1/p' $configfile)
+if [[ -z "$port" ]]; then
+  echo "Port for healthcheck not available."
+  exit 4
+fi
+
 healthcheckcommand="curl --fail -v"
 if [ "$protocol" == "https" ]; then
     healthcheckcommand="$healthcheckcommand --insecure"
