8c2d50ea21939ae705defd8c9382ab7fbeefbea4
Marius Göcke <marius.goecke@gmail.com>
2026-04-03 12:49:32 +0200

    Added multi-arch-support.

--- OpenDMS/Other/Build/Build.py
+++ OpenDMS/Other/Build/Build.py
@@ -1,9 +1,14 @@
+from ScriptCollection.GeneralUtilities import Platform
 from ScriptCollection.TFCPS.Docker.TFCPS_CodeUnitSpecific_Docker import TFCPS_CodeUnitSpecific_Docker_Functions,TFCPS_CodeUnitSpecific_Docker_CLI
 
  
 def build():
+    platforms:list[Platform] = [
+            Platform.Linux_AMD64,
+            Platform.Linux_ARM64,
+    ]
     tf:TFCPS_CodeUnitSpecific_Docker_Functions=TFCPS_CodeUnitSpecific_Docker_CLI.parse(__file__)
-    tf.build({
+    tf.build(platforms,{
         "image_debian":tf.tfcps_Tools_General.oci_image_manager.get_registry_address_for_image_with_default_tag(tf.get_repository_folder(),"Debian"),
     })
     tf.tfcps_Tools_General.merge_sbom_file_from_dependent_codeunit_into_this(tf.get_codeunit_folder(),tf.get_codeunit_name(),"OpenDMSBackend",tf.use_cache())
--- OpenDMSBackend/Other/Build/Build.py
+++ OpenDMSBackend/Other/Build/Build.py
@@ -1,13 +1,18 @@
+from ScriptCollection.GeneralUtilities import GeneralUtilities, Platform
 from ScriptCollection.TFCPS.DotNet.TFCPS_CodeUnitSpecific_DotNet import TFCPS_CodeUnitSpecific_DotNet_Functions,TFCPS_CodeUnitSpecific_DotNet_CLI
  
 def build():
-
-    platforms = ["win-x64", "linux-x64"]
+    platforms:list[Platform] = [
+            Platform.Windows_AMD64,
+            Platform.Linux_AMD64,
+            Platform.Linux_ARM64,
+    ]
     tf:TFCPS_CodeUnitSpecific_DotNet_Functions=TFCPS_CodeUnitSpecific_DotNet_CLI.parse(__file__)
-    tf.build(platforms, True) 
+    tf.build([GeneralUtilities.platform_to_dotnet_runtime_identifier(p)  for p in platforms], True)
     for platform in platforms:
         pass# TODO copy codeunit-resource "TypeScript" to output-directory
     # TODO add typescript to sbom
-    
+
+
 if __name__ == "__main__":
     build()
